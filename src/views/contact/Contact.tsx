f3f1d5260ac83c9177a5fcbe1ca5be9415105cbe
RyanLindman <ryan.lindman@outlook.com>
2024-05-17 20:12:14 +0200

    publishing

--- src/views/contact/Contact.tsx
+++ src/views/contact/Contact.tsx
@@ -41,21 +41,21 @@ const Contact = () => {
             sx={{
               display: 'flex',
               background: '#323232',
-              padding: 10,
+              padding: isSmallScreen ? '6px' : 20,
               flexDirection: 'column',
               gap: 2,
               alignItems: 'center',
             }}
           >
-            <Box display={'flex'} alignItems={'center'} gap={1}>
+            <Box display={'flex'} gap={1}>
               <EmailIcon fontSize="large" color="secondary" />
               <Typography variant="h4">ryan.lindman@outlook.com</Typography>
             </Box>
-            <Box display={'flex'} alignItems={'center'} gap={1}>
+            <Box display={'flex'} gap={1}>
               <PhoneIcon fontSize="large" color="secondary" />
               <Typography variant="h4">+(46)728 59 34 60</Typography>
             </Box>
-            <Box display={'flex'} alignItems={'center'} gap={1}>
+            <Box display={'flex'} gap={1}>
               <a
                 href="https://www.linkedin.com/in/ryan-lindman-0bb377276/"
                 rel="noopener noreferrer"
